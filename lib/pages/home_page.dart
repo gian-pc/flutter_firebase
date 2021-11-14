e3aa771d307ba304b7c11ed4ce87f5c03a71a2c7
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-14 12:21:44 -0500

    getAllDataFirebase - limitToLast

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,7 +19,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   getAllDataFirebase() {
-    bandCollection.orderBy('band').get().then((value) {
+    bandCollection.orderBy('id').limitToLast(2).get().then((value) {
       value.docs.forEach((element) {
         Map<String, dynamic> myMap = element.data() as Map<String, dynamic>;
         myBands.add(myMap);
