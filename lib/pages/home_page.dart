8ff3e562d071f69b23e91d1539e0ac547b92d267
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-14 00:16:46 -0500

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,123 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  CollectionReference bandCollection =
+      FirebaseFirestore.instance.collection('bandas');
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getAllDataFirebase();
+  }
+
+  getAllDataFirebase() {
+    print("Message:::::$bandCollection");
+    bandCollection.get().then((value) {
+      print("value::::::${value.docs.runtimeType}");
+      value.docs.forEach((element) {
+        //print("Document:::::${element.data()}");
+        Map<String, dynamic> myMap = element.data() as Map<String, dynamic>;
+        print(myMap);
+      });
+    });
+  }
+
+  getDocumentFirebase(){
+    bandCollection.doc("mVCbEnqjZ8rShpQIGlB4").get().then((value){
+
+      if(value.exists){
+        print("Band::::::${value.data()}");
+      }else{
+        print("La banda no existe");
+      }
+
+
+    });
+  }
+
+
+  addDocumentFirebase(){
+    bandCollection.add({
+      'id':6,
+      'band':'Metronomyssssss',
+      'image':'https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/3/6/7/f/367f472896e70c11cadc0efe791870c7.jpg',
+      'status':true,
+
+    }).then((value) {
+      print("Banda agregada");
+    }).catchError((error){
+      print("Hubo un error");
+    });
+
+  }
+
+  addDocumentIdFirebase(){
+    bandCollection.doc("MandarinaQW323").set({
+      'id':5,
+      'band':'Disturbed',
+      'image':'https://www.todorock.com/wp-content/uploads/2019/09/disturbed-1200x900.jpeg',
+      'status':true,
+    }).then((value) {
+      print("Banda agregada");
+    }).catchError((error){
+      print("Hubo un error");
+    });
+  }
+
+  updateDocumentFirebase(){
+    bandCollection.doc("kqh8x0XMqjEbhCHsoi5g").update({
+      'id':6,
+      'band':'Belle and Sebastian',
+      'image':'https://upload.wikimedia.org/wikipedia/commons/1/19/Belle_and_Sebastian_British_Band.jpeg',
+      'status':true,
+    }).then((value) {
+      print("Banda actualizada");
+    }).catchError((error){
+      print("Hubo un error");
+    });
+  }
+
+  deleteDocumentFirebase(){
+   bandCollection.doc("nqQjo7gIDRvmQ2SReqIr").delete().then((value) {
+     print("Banda eliminada");
+   }).catchError((error){
+     print("Hubo un error al eliminar");
+   });
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Example"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              getDocumentFirebase();
+            },
+            icon: Icon(Icons.search),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          //addDocumentFirebase();
+          //addDocumentIdFirebase();
+          //updateDocumentFirebase();
+          deleteDocumentFirebase();
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Center(),
+    );
+  }
+}
