7ac166387d60bba793dcaf1982070ca9adbfc038
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-14 00:31:00 -0500

    Listando bandas

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,6 +7,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Map> myBands = [];
   CollectionReference bandCollection =
       FirebaseFirestore.instance.collection('bandas');
 
@@ -24,75 +25,71 @@ class _HomePageState extends State<HomePage> {
       value.docs.forEach((element) {
         //print("Document:::::${element.data()}");
         Map<String, dynamic> myMap = element.data() as Map<String, dynamic>;
-        print(myMap);
+        myBands.add(myMap);
+        setState(() {});
       });
     });
   }
 
-  getDocumentFirebase(){
-    bandCollection.doc("mVCbEnqjZ8rShpQIGlB4").get().then((value){
-
-      if(value.exists){
+  getDocumentFirebase() {
+    bandCollection.doc("mVCbEnqjZ8rShpQIGlB4").get().then((value) {
+      if (value.exists) {
         print("Band::::::${value.data()}");
-      }else{
+      } else {
         print("La banda no existe");
       }
-
-
     });
   }
 
-
-  addDocumentFirebase(){
+  addDocumentFirebase() {
     bandCollection.add({
-      'id':6,
-      'band':'Metronomyssssss',
-      'image':'https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/3/6/7/f/367f472896e70c11cadc0efe791870c7.jpg',
-      'status':true,
-
+      'id': 6,
+      'band': 'Metronomyssssss',
+      'image':
+          'https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/3/6/7/f/367f472896e70c11cadc0efe791870c7.jpg',
+      'status': true,
     }).then((value) {
       print("Banda agregada");
-    }).catchError((error){
+    }).catchError((error) {
       print("Hubo un error");
     });
-
   }
 
-  addDocumentIdFirebase(){
+  addDocumentIdFirebase() {
     bandCollection.doc("MandarinaQW323").set({
-      'id':5,
-      'band':'Disturbed',
-      'image':'https://www.todorock.com/wp-content/uploads/2019/09/disturbed-1200x900.jpeg',
-      'status':true,
+      'id': 5,
+      'band': 'Disturbed',
+      'image':
+          'https://www.todorock.com/wp-content/uploads/2019/09/disturbed-1200x900.jpeg',
+      'status': true,
     }).then((value) {
       print("Banda agregada");
-    }).catchError((error){
+    }).catchError((error) {
       print("Hubo un error");
     });
   }
 
-  updateDocumentFirebase(){
+  updateDocumentFirebase() {
     bandCollection.doc("kqh8x0XMqjEbhCHsoi5g").update({
-      'id':6,
-      'band':'Belle and Sebastian',
-      'image':'https://upload.wikimedia.org/wikipedia/commons/1/19/Belle_and_Sebastian_British_Band.jpeg',
-      'status':true,
+      'id': 6,
+      'band': 'Belle and Sebastian',
+      'image':
+          'https://upload.wikimedia.org/wikipedia/commons/1/19/Belle_and_Sebastian_British_Band.jpeg',
+      'status': true,
     }).then((value) {
       print("Banda actualizada");
-    }).catchError((error){
+    }).catchError((error) {
       print("Hubo un error");
     });
   }
 
-  deleteDocumentFirebase(){
-   bandCollection.doc("nqQjo7gIDRvmQ2SReqIr").delete().then((value) {
-     print("Banda eliminada");
-   }).catchError((error){
-     print("Hubo un error al eliminar");
-   });
+  deleteDocumentFirebase() {
+    bandCollection.doc("nqQjo7gIDRvmQ2SReqIr").delete().then((value) {
+      print("Banda eliminada");
+    }).catchError((error) {
+      print("Hubo un error al eliminar");
+    });
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -109,7 +106,7 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           //addDocumentFirebase();
           //addDocumentIdFirebase();
           //updateDocumentFirebase();
@@ -117,7 +114,30 @@ class _HomePageState extends State<HomePage> {
         },
         child: Icon(Icons.add),
       ),
-      body: Center(),
+      body: ListView.builder(
+        itemCount: myBands.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 360,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(myBands[index]["image"]))),
+            child: Center(
+              child: Text(
+                myBands[index]["band"],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40.0,
+                  letterSpacing: 10,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
